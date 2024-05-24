60f3f932947df0e2bbcb13852444d9134b12e0cd
kimjeongmin <kij9451@naver.com>
2024-05-24 15:05:26 +0900

    feat: 동아리 등록된 거 조회하는 거 임시로 만듦. 동아리 가입 신청 완료

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import { RecoilRoot } from "recoil";
 import NoticeDetail from "./club/master/NoticeDetail";
 import Video from "./club/common/Video";
 import CreateVideo from "./club/master/CreateVideo"; 
+import ClubJoin from "./club/member/ClubJoin";
 
 
 
@@ -36,6 +37,7 @@ function App() {
         <Route path="/NoticeDetail/:id" element={<NoticeDetail />} />
         <Route path="/CreateVideo" element={<CreateVideo />} />
         <Route path="/Video" element={<Video />} />
+        <Route path="/ClubJoin/:id" element={<ClubJoin />} />
       </Routes>
     </Router>
     </RecoilRoot>
--- src/club/master/CreateNotice.js
+++ src/club/master/CreateNotice.js
@@ -1,29 +1,61 @@
-import { Grid, Typography } from "@mui/material";
-import ButtonAppBar from "../../common/MainAppBar";
-import Editor from "./Editor";
-import CreateNoticeHeader from "./CreateNoticeHeader";
-import Title from "./Title";
-import { useNavigate } from "react-router";
-import { useLocation, useMatch } from "react-router-dom";
-import FileUploader from "./FlieUploader";
+import React, { useEffect, useState } from 'react';
+import { useRecoilState } from 'recoil';
+import axios from 'axios';
+import { Grid, Typography, Dialog, DialogTitle, DialogContent, DialogActions, List, ListItem, ListItemText, Button } from '@mui/material';
+import ButtonAppBar from '../../common/MainAppBar';
+import Editor from './Editor';
+import CreateNoticeHeader from './CreateNoticeHeader';
+import Title from './Title';
+import { useNavigate } from 'react-router';
+import { useLocation, useMatch } from 'react-router-dom';
+import FileUploader from './FlieUploader';
+import { clubIdState } from '../../recoil/state/clubState';
+import ClubDialog from './clubListDialog';
+import instance from '../../api/instance';
 
 export default function CreateNotice() {
   const location = useLocation();
+  const [open, setOpen] = useState(false);
+  const [clubs, setClubs] = useState([]);
+  const [clubId, setClubIdState] = useRecoilState(clubIdState);
 
   const { notice } = location.state || {};
   const { recruitment } = location.state || {};
-  const matchUpdate = useMatch("/UpdateNotice/:id");
-  const matchRecruitmentUpdate = useMatch("/UpdateMemberRecruitment/:id");
-  const CreateMemberRecruitment = useMatch("/CreateMemberRecruitment");
-  console.log("Editnotice", notice);
-  console.log("useMatch", matchUpdate);
-  console.log("hihi", matchRecruitmentUpdate);
+  const matchUpdate = useMatch('/UpdateNotice/:id');
+  const matchRecruitmentUpdate = useMatch('/UpdateMemberRecruitment/:id');
+  const CreateMemberRecruitment = useMatch('/CreateMemberRecruitment');
   const noticeData = matchUpdate && notice ? notice : null;
-  const recruitmentData = matchRecruitmentUpdate && recruitment ? recruitment: null;
-  console.log("noticenotice", noticeData);
-  console.log("ddurlsedj", recruitmentData);
+  const recruitmentData = matchRecruitmentUpdate && recruitment ? recruitment : null;
+  console.log("clubs", clubs)
+
+
+  console.log("clubId", clubId)
+
+  useEffect(() => {
+    instance
+        .get('/club')
+        .then((response) => {
+          setClubs(response?.data);
+          console.log("noticeList", response?.data)
+        })
+        .catch((error) => console.error(error));
+}, []);  
+
+  
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleClubSelect = (clubId) => {
+    setClubIdState(clubId);
+    // setOpen(false);
+  };
   return (
-    <Grid container direction={"column"} spacing={1}>
+    <Grid container direction={'column'} spacing={1}>
       <Grid item xs={12}>
         <ButtonAppBar />
       </Grid>
@@ -38,10 +70,16 @@ export default function CreateNotice() {
         {notice ? <Title notice={noticeData} /> : <Title notice={recruitmentData} />}
       </Grid>
       <Grid item xs={12}>
-       {matchRecruitmentUpdate || CreateMemberRecruitment?         <FileUploader/> :        ""}
+        {matchRecruitmentUpdate || CreateMemberRecruitment ? <FileUploader /> : ''}
       </Grid>
       <Grid item xs={12}>
-        {notice? (<Editor notice={noticeData} />): (<Editor notice={recruitmentData} />)}
+        {notice ? <Editor notice={noticeData} /> : <Editor notice={recruitmentData} />}
+      </Grid>
+      <Grid item xs={12}>
+        <ClubDialog open={open} handleClose={handleClose} clubs={clubs} handleClubSelect={handleClubSelect} />
+        <Button onClick={handleOpen}>{clubId.length > 0
+            ? `동아리: ${clubId}`
+            : '동아리'}</Button>
       </Grid>
     </Grid>
   );
